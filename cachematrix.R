51bb267d7b03ec71a6bab84ce79957c5454ad37c
markrnaustin <markrnaustin@gmail.com>
2015-08-22 14:35:44 -0500

    Completed and tested script includes appropriate functions as well as several lines to demonstrate the validity of the work.

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,31 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    s <- NULL
+    
+    ## basic setter: set matrix data value
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    
+    ## basic getter : return matrix data value
+    get <- function() {
+        x
+    }
+    
+    ## basic setter : set the local inverted matrix
+    setInverse <- function(solve) {
+        s <<- solve
+    }
+    
+    ## basic getter : return the cached inverted matrix
+    getInverse <- function() {
+        s
+    }
+    
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+    
 }
 
 
@@ -12,4 +37,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    s <- x$getInverse()
+    
+    if (!is.null(s)) {
+        message("getting cached data")
+        return(s)
+    }
+    
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setInverse(s)
+    s
 }
+
+gg <- makeCacheMatrix(matrix(c(4,3,3,2), nrow=2, ncol=2, byrow=TRUE))
+cacheSolve(gg)  ## this one will cache (first time for this matrix)
+cacheSolve(gg)  ## this one will USE cache
