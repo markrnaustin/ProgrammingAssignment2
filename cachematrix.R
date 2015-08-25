b0ecc58aeebafb4694d42039459804ef935379f4
markrnaustin <markrnaustin@gmail.com>
2015-08-24 19:18:20 -0500

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a special matrix object that holds the actual matrix as a member variable and inverse
+## of it as another member variable and provides getter/setter methods. Simply put, makeCacheMatrix is an object 
+## encapsulating the matrix and the inverse.
 
-## Write a short comment describing this function
+## Parameter: x - Matrix to be encapsulated - the inverse of which should be cached.
+## Return the special matrix that encapsulates 'x' and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -33,7 +35,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates the inverse of the matrix created by makeCacheMatrix. It first checks if there is a cached 
+## version of inverse of the matrix. If there exists a cached copy, it returns the same; Otherwise, it calculates
+## the inverse and stores the inverse in the special matrix.
+
+## Parameter: x - Matrix object obtained by makeCacheMatrix method
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -50,6 +57,7 @@ cacheSolve <- function(x, ...) {
     s
 }
 
+## some test code of a simple 2x2 matrix
 gg <- makeCacheMatrix(matrix(c(4,3,3,2), nrow=2, ncol=2, byrow=TRUE))
 cacheSolve(gg)  ## this one will cache (first time for this matrix)
 cacheSolve(gg)  ## this one will USE cache
